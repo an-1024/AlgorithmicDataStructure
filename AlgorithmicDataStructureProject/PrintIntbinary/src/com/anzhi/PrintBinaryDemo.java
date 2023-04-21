83ff311e4a45a5cfa2ec15f775fa467cb03d5be6
azh <2550364626@qq.com>
2023-04-21 21:34:32 +0800

    打印整数的二进制

--- AlgorithmicDataStructureProject/PrintIntbinary/src/com/anzhi/PrintBinaryDemo.java
+++ AlgorithmicDataStructureProject/PrintIntbinary/src/com/anzhi/PrintBinaryDemo.java
@@ -4,10 +4,20 @@ package com.anzhi;
  * 打印一个整数的二进制
  */
 public class PrintBinaryDemo {
-    public void printIntBinary(int num) {
+    public static void printIntBinary(int num) {
         for (int i=31; i>=0; i--) {
-            System.out.print((num & (1 << i)) == 0 ? "0" : 1);
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+            if(i%4 == 0) {
+                System.out.print(" ");
+            }
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int num = -2147483648;
+        printIntBinary(num);
+        System.out.println(~num);
+        printIntBinary(~num);
     }
 }
