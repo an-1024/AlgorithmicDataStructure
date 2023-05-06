75aa896b1926f7d4c7377cde6b51256299100244
azh <2550364626@qq.com>
2023-05-06 16:15:08 +0800

    单链表翻转

--- AlgorithmicDataStructureProject/link-structure/src/main/java/com/anzhi/ReverseListDemo.java
+++ AlgorithmicDataStructureProject/link-structure/src/main/java/com/anzhi/ReverseListDemo.java
@@ -3,11 +3,13 @@ package com.anzhi;
 public class ReverseListDemo {
     public static class Node {
         public int value;
-        public Node next;
+        public Node next = null;
         
         public Node (int data) {
             this.value = data;
         }
+        
+        public Node (){}
     }
     
     public static class DoubleNode {
@@ -18,5 +20,47 @@ public class ReverseListDemo {
         public DoubleNode (int data) {
             this.value = data;
         }
+        
+        public DoubleNode(){}
     }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        
+        n1.next = n2;
+        n2.next = n3;
+
+        System.out.print("未翻转之前的单链表: ");
+        printNode(n1);
+        System.out.print("翻转之后的单链表: ");
+        Node reversalNode = reversalLinkNode(n1);
+        printNode(reversalNode);
+    }
+
+    private static Node reversalLinkNode(Node node) {
+        Node pre = null;
+        Node next;
+        
+        while (node != null) {
+            next = node.next;
+            node.next = pre;
+            pre = node;
+            node = next;
+        }
+        
+        return pre;
+    }
+
+
+    public static void printNode(Node node) {
+        while (node != null) {
+            System.out.print(" " + node.value);
+            node = node.next;
+        }
+        System.out.println();
+    }
+    
+    
 }
