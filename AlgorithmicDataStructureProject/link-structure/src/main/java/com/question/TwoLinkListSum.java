3835b8ca580120a4ab7982886ce510b4e5d4101e
azh <2550364626@qq.com>
2023-06-30 10:15:16 +0800

    两个链表相加

--- AlgorithmicDataStructureProject/link-structure/src/main/java/com/question/TwoLinkListSum.java
+++ AlgorithmicDataStructureProject/link-structure/src/main/java/com/question/TwoLinkListSum.java
@@ -8,4 +8,64 @@ package com.question;
  */
 public class TwoLinkListSum {
     
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+        
+        public ListNode (int val) {
+            this.val = val;
+        }
+    }
+    
+    public static ListNode addTwoNumbers(ListNode head1,  ListNode head2) {
+        int len1 = listLength(head1);
+        int len2 = listLength(head2);
+        
+        // 区分长短链表
+        ListNode l = len1 >= len2 ? head1 : head2;
+        ListNode s = l == head1 ? head2 : head1;
+        
+        ListNode curL = l;
+        ListNode curS = s;
+        ListNode last = curL;
+        
+        // 进位标志
+        int carry = 0;
+        int curNum = 0;
+        
+        // 先做短链表
+        while (curS != null) {
+            curNum = curL.val + curS.val + carry;
+            curL.val = (curNum % 10);
+            carry = curNum / 10;
+            last = curL;
+            curL = curL.next;
+            curS = curS.next;
+        }
+        
+        // 短链表计算完成，长链表进行进位计算
+        while (curL != null) {
+            curNum = curL.val + carry;
+            curL.val = (curNum % 10);
+            carry = curNum / 10;
+            last = curL;
+            curL = curL.next;
+        }
+        
+        // 如果进位不为0，那么需要创建一个节点放到尾部
+        if (carry != 0) {
+            last.next = new ListNode(1);
+        }
+        return l;
+    }
+    
+    // 求链表长度
+    public static int listLength(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
 }
